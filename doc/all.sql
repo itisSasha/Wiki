faada14ddf499d9aaf93f784ec21ae5995cc40e8
li-xuan <li-xuan@smfl.co.jp>
2022-08-22 13:22:56 +0900

    2-9 集成mybatis generateor, 生产demo表代码

---
+++ doc/all.sql
@@ -0,0 +1,19 @@
+drop table if exists `test`;
+create table `test` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `password` varchar(50) comment 'password',
+    primary key(`id`)
+) engine=innodb default charset=utf8mb4 comment='test';
+
+insert into `test` (id, name, password) values (1, 'test', 'password');
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key(`id`)
+) engine=innodb default charset=utf8mb4 comment='test';
+
+insert into `demo` (id, name) values (1, 'test');
+
