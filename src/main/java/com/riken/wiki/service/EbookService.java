78a397825832d403171910a463a484f0d3343b64
li-xuan <li-xuan@smfl.co.jp>
2022-08-23 10:57:09 +0900

    3-2 制作CopyUtil封装BeanUtil

--- src/main/java/com/riken/wiki/service/EbookService.java
+++ src/main/java/com/riken/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.riken.wiki.domain.EbookExample;
 import com.riken.wiki.mapper.EbookMapper;
 import com.riken.wiki.req.EbookReq;
 import com.riken.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.riken.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,14 +24,19 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-//            ebookResp.setId(123L);   テスト用
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            //            ebookResp.setId(123L);   テスト用
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//        Objectのコピー
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+
+//        listのコピー
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 
 }
