4666cf5da1387ecbd0d758ee13c8a58d2a72cff0
li-xuan <li-xuan@smfl.co.jp>
2022-08-28 14:36:24 +0900

    4-1 使用ref和reactive实现数据绑定

--- src/main/java/com/riken/wiki/Config/CorsConfig.java
+++ src/main/java/com/riken/wiki/Config/CorsConfig.java
@@ -1,9 +1,11 @@
 package com.riken.wiki.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
