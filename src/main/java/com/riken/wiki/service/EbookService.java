dbf0c6c0e99c063d3380e1a4fde76a7f3f937b7d
li-xuan <li-xuan@smfl.co.jp>
2022-08-29 21:21:23 +0900

    4-3 列表查询接口支持动态sql

--- src/main/java/com/riken/wiki/service/EbookService.java
+++ src/main/java/com/riken/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.riken.wiki.req.EbookReq;
 import com.riken.wiki.resp.EbookResp;
 import com.riken.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -21,7 +22,9 @@ public class EbookService {
         EbookExample ebookExample = new EbookExample();
 //        Criteriaはwhereの意味
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
 //        List<EbookResp> respList = new ArrayList<>();
