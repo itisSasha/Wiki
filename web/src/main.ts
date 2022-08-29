e262b9f4da72784cadbbc8173ead4faa43a8f794
li-xuan <li-xuan@smfl.co.jp>
2022-08-29 21:36:53 +0900

    4-4 Vue多环境配置，增加dev和prod配置文件

--- web/src/main.ts
+++ web/src/main.ts
@@ -14,3 +14,6 @@ const app = createApp(App);
     for(const i in icons){
         app.component(i,icons[i]);
     }
+
+    console.log("環境", process.env.NODE_ENV);
+    console.log("サーバー", process.env.VUE_APP_SERVER);
